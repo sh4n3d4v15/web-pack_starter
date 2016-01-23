c63bd776212b6ccb7df5614d521ac217091ca190
shane davis <Shane@shanes-MacBook-Air.local>
2016-01-23 14:11:24 +0000

    css-hotload

--- webpack.config.js
+++ webpack.config.js
@@ -3,54 +3,56 @@ const merge = require('webpack-merge');
 const TARGET = process.env.npm_lifecycle_event;
 const webpack = require('webpack');
 const PATHS = {
-  app: path.join(__dirname, 'app'),
-  build: path.join(__dirname, 'build')
+    app: path.join(__dirname, 'app'),
+    build: path.join(__dirname, 'build')
 };
 
 const common = {
-  // Entry accepts a path or an object of entries.
-  // The build chapter contains an example of the latter.
-  entry: PATHS.app,
-  output: {
-    path: PATHS.build,
-    filename: 'bundle.js'
-  }
+    // Entry accepts a path or an object of entries.
+    // The build chapter contains an example of the latter.
+    entry: PATHS.app,
+    output: {
+        path: PATHS.build,
+        filename: 'bundle.js'
+    }
 };
 
 // Default configuration
-if(TARGET === 'start' || !TARGET) {
+if (TARGET === 'start' || !TARGET) {
 
-  module.exports = merge(common, {});
+    module.exports = merge(common, {
+        devServer: {
+            contentBase: PATHS.build,
 
+            // Enable history API fallback so HTML5 History API based
+            // routing works. This is a good default that will come
+            // in handy in more complicated setups.
+            historyApiFallback: true,
+            hot: true,
+            inline: true,
+            progress: true,
 
-  module.exports = merge(common, {
-    devServer: {
-      contentBase: PATHS.build,
+            // Display only errors to reduce the amount of output.
+            stats: 'errors-only',
 
-      // Enable history API fallback so HTML5 History API based
-      // routing works. This is a good default that will come
-      // in handy in more complicated setups.
-      historyApiFallback: true,
-      hot: true,
-      inline: true,
-      progress: true,
-
-      // Display only errors to reduce the amount of output.
-      stats: 'errors-only',
-
-      // Parse host and port from env so this is easy to customize.
-      host: process.env.HOST,
-      port: process.env.PORT
-    },
-    plugins: [
-      new webpack.HotModuleReplacementPlugin()
-    ]
-  });
+            // Parse host and port from env so this is easy to customize.
+            host: process.env.HOST,
+            port: process.env.PORT
+        },
+        module: {
+            loaders: [{
+                test: /\.css$/,
+                loaders: ['style', 'css'],
+                include: PATHS.app
+            }]
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin()
+        ]
+    });
 }
 
 
-if(TARGET === 'build') {
-  module.exports = merge(common, {});
+if (TARGET === 'build') {
+    module.exports = merge(common, {});
 }
-
-
