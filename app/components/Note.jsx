dac4003f57418e340d27d30f4f210c7bb1421352
shane davis <Shane@shanes-MacBook-Air.local>
2016-01-23 17:35:20 +0000

    hmre

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,3 +1,3 @@
 import React from 'react';
 
-export default () => <div>LEARN WEBPACK</div>
+export default () => <div>Miniman</div>
--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,65 @@
-const path = require('path')
+const path = require('path');
 const merge = require('webpack-merge');
-const TARGET = process.env.npm_lifecycle_event;
 const webpack = require('webpack');
+
+const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
-    app: path.join(__dirname, 'app'),
-    build: path.join(__dirname, 'build')
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
 };
+
+process.env.BABEL_ENV = TARGET;
 
 const common = {
-    // Entry accepts a path or an object of entries.
-    // The build chapter contains an example of the latter.
-    entry: PATHS.app,
-    resolve: {
-      extensions: ['','.js','.jsx']
-    },
-    output: {
-        path: PATHS.build,
-        filename: 'bundle.js'
-    }
+  entry: PATHS.app,
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: PATHS.app
+      },
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel?cacheDirectory'],
+        include: PATHS.app
+      }
+    ]
+  }
 };
 
-// Default configuration
-if (TARGET === 'start' || !TARGET) {
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
+      contentBase: PATHS.build,
 
-    module.exports = merge(common, {
-        devtool: 'eval-source-map',
-        devServer: {
-            contentBase: PATHS.build,
-            // Enable history API fallback so HTML5 History API based
-            // routing works. This is a good default that will come
-            // in handy in more complicated setups.
-            historyApiFallback: true,
-            hot: true,
-            inline: true,
-            progress: true,
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
 
-            // Display only errors to reduce the amount of output.
-            stats: 'errors-only',
+      // display only errors to reduce the amount of output
+      stats: 'errors-only',
 
-            // Parse host and port from env so this is easy to customize.
-            host: process.env.HOST,
-            port: process.env.PORT
-        },
-        module: {
-            loaders: [{
-                test: /\.css$/,
-                loaders: ['style', 'css'],
-                include: PATHS.app
-            },{
-              test: /\.jsx?$/,
-              loaders: ['babel?cacheDirectory'],
-              incldue: PATHS.app
-            }]
-        },
-        plugins: [
-            new webpack.HotModuleReplacementPlugin()
-        ]
-    });
+      // parse host and port from env so this is easy
+      // to customize
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
 }
 
-
-if (TARGET === 'build') {
-    module.exports = merge(common, {});
+if(TARGET === 'build') {
+  module.exports = merge(common, {});
 }
