3b53f707092ddffdd87addc5f602b1cfaf8de706
shane davis <Shane@shanes-MacBook-Air.local>
2016-01-24 07:56:14 +0000

    local-storage-alt

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,50 @@
-import React from 'react';
-import Note from './Note.jsx';
+import React 		from 'react';
+import Notes 		from './Notes.jsx';
+
+import NoteActions 	from '../actions/NoteActions';
+import NoteStore 	from '../stores/NoteStore';
 
 export default class App extends React.Component{
+
+	constructor(props){
+		super(props)
+
+		this.state = NoteStore.getState();
+	};
+
+	componentDidMount(){
+		NoteStore.listen(this.storeChanged);
+	};
+	componentWillUnmount(){
+		NoteStore.unlisten(this.storeChanged)
+	};
+	storeChanged = (state) =>{
+		this.setState(state);
+	};
 	render(){
-		return <Note />
-	}
+
+	const notes = this.state.notes
+	return (
+
+		<div>
+			<button className="add-note" onClick={this.addNote}>+</button>
+
+			<Notes notes={notes} 
+			onEdit={this.editNote}
+			onDelete={this.deleteNote} />
+		</div>
+	)
+	};
+
+	deleteNote = (id) =>{
+		NoteActions.delete(id);
+	};
+
+	editNote = (id, task) =>{
+		NoteActions.update({id,task});
+	};
+
+	addNote = () => {
+		NoteActions.create({task:'New Task'});
+	};
 }
--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,12 @@
 import './main.css'
 
-import React from 'react';
-import ReactDom from 'react-dom';
-import App from './components/App.jsx';
+import React 		from 'react';
+import ReactDom 	from 'react-dom';
+import App 			from './components/App.jsx';
 
+import alt 			from './libs/alt';
+import storage		from './libs/storage';
+import persist 		from './libs/persist'
+
+persist(alt , storage , 'app');
 ReactDom.render(<App />, document.getElementById('app'))
